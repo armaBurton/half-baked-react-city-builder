153254b764755615606509bdf4d435f00bdb3d62
armaBurton <armanarma@armaburton.com>
2022-01-31 13:00:46 -0800

    lunchtime, pushing progress

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import SkylineDropdown from './SkylineDropdown';
 import WaterfrontDropdown from './WaterfrontDropdown';
 import CastleDropdown from './CastleDropdown';
-import SloganList from './SloganList';
+// import SloganList from './SloganList';
 import SloganForm from './SloganForm';
 import CityNameInput from './CityNameInput';
 import City from './City';
@@ -11,28 +11,42 @@ import City from './City';
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
+  const [waterfrontId, setWaterfrontId] = useState(1);
+  const [skylineId, setSkylineId] = useState(1);
+  const [castleId, setCastleId] = useState(1);
+  const [cityName, setCityName] = useState(`Portland`);
+  const [slogan, setSlogan] = useState([]);
+
   // you'll need to track a city name, which starts as the city name of your choice.
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
 
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      <City 
+        waterfrontId={waterfrontId}
+        skylineId={skylineId}
+        castleId={castleId}
+      />
       <h1>
         {/* dynamically update the city name here using state */}
-        Welcome to beautiful Portland!
+        Welcome to beautiful {cityName}!
       </h1>
       <div className='bottom'>
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
+        <CityNameInput setCityName={setCityName}/>
         <section className='dropdowns'>
           {/* 
           render all three Dropdown components (WaterfrontDropdown, SkylineDropdown, CastleDropdown) here. 
-          
           In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state 
-          */}
+        */}
+          <WaterfrontDropdown setWaterfrontId={setWaterfrontId} />
+          <SkylineDropdown setSkylineId={setSkylineId} />
+          <CastleDropdown setCastleId={setCastleId}/>
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
         {/* here, the SloganList component takes the array of slogans that lives in state */}
-
+        <SloganForm slogan={slogan} setSlogan={setSlogan} />
       </div>
     </div>
   );
--- src/SloganForm.js
+++ src/SloganForm.js
@@ -1,14 +1,24 @@
-export default function SloganForm(props) {
+import { useState } from 'react';
+
+export default function SloganForm(slogan, setSlogan) {
   // React forms are a pain! 
   // Track the sloganInput form state with a useState hook
+  const [sloganInForm, setSloganInForm] = useState(``);
     
   function handleSubmit(e) {
     e.preventDefault();
 
+    console.log(sloganInForm);
+    
     // This component takes in a prop called setSlogans, which is a function that takes in an array of slogans. 
+    // setSlogan([...slogan, sloganInForm]);
     // Also, this component also takes in a prop called slogans, which is an arry of strings. //// On submit call the setSlogans state with a new array that is a copy of the old array with the new slogan immutably added to the end 
-
+    
     // set the sloganInput form state to an empty string to reset the form
+  }
+  
+  function handleSloganChange(e){
+    setSloganInForm(e.target.value);
   }
 
   return (
@@ -18,7 +28,7 @@ export default function SloganForm(props) {
         Add a slogan to the list!
         {/* on change, set the sloganInput in state to be the input value */}
         {/* also, weirdly, set the value of this input to the sloganInput tracked in state to make this a 'controlled' form input */}
-        <input />
+        <input required value={sloganInForm} onChange={handleSloganChange} />
         <button>Submit</button>
       </form>
     </section>
